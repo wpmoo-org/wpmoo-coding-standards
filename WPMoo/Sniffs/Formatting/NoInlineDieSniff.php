1eac8b14c3a849009087fe375e6c320192650f12
Ahmet Cangir <ahmetcangir07@gmail.com>
2025-10-28 14:59:42 +0100

    Refactor NoInlineDieSniff class for improved code consistency and formatting

--- WPMoo/Sniffs/Formatting/NoInlineDieSniff.php
+++ WPMoo/Sniffs/Formatting/NoInlineDieSniff.php
@@ -8,32 +8,35 @@ use PHP_CodeSniffer\Sniffs\Sniff;
 /**
  * Disallow direct usage of die()/exit() in favour of wp_die().
  */
-class NoInlineDieSniff implements Sniff
-{
-    /**
-     * {@inheritdoc}
-     */
-    public function register(): array
-    {
-        return [T_EXIT];
-    }
+class NoInlineDieSniff implements Sniff {
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @return array<int, int>
+	 */
+	public function register(): array {
+		return [ T_EXIT ];
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function process(File $phpcsFile, $stackPtr)
-    {
-        $tokens = $phpcsFile->getTokens();
-        $token = $tokens[$stackPtr];
-        $content = strtolower($token['content']);
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @param File $phpcs_file The file being scanned.
+	 * @param int  $stack_ptr  The position of the current token in the token stack.
+	 * @return void
+	 */
+	public function process( File $phpcs_file, $stack_ptr ) {
+		$tokens  = $phpcs_file->getTokens();
+		$token   = $tokens[ $stack_ptr ];
+		$content = strtolower( $token['content'] );
 
-        if ($content === 'exit' || $content === 'die') {
-            $phpcsFile->addError(
-                'Use wp_die() instead of %s().',
-                $stackPtr,
-                'FoundDie',
-                [$content]
-            );
-        }
-    }
+		if ( 'exit' === $content || 'die' === $content ) {
+			$phpcs_file->addError(
+				'Use wp_die() instead of %s().',
+				$stack_ptr,
+				'FoundDie',
+				[ $content ]
+			);
+		}
+	}
 }
